f496cfbb1555258a4c465a9805e15939076f5631
Falasefemi2 <femifalase228@gmail.com>
2024-09-20 20:18:53 +0100

    added bg-primary to sign-in page

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -4,7 +4,7 @@ import { dark } from '@clerk/themes'
 
 export default function Page() {
     return (
-        <div className="w-full min-h-screen flex justify-center items-center">
+        <div className="w-full min-h-screen flex justify-center items-center bg-primary">
             <SignIn
                 appearance={{
                     baseTheme: dark,
